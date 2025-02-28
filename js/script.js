74703447ee0b44a1416f2e088379880e7a7caef6
Bruno <bruno9405@fito.br>
2025-02-28 19:51:22 -0300

    Adicionando Responsividade

--- js/script.js
+++ js/script.js
@@ -24,3 +24,19 @@ function toggleMenu() {
 }
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.getElementById("menu-icon");
+    const sidebar = document.getElementById("sidebar");
+    const closeBtn = document.getElementById("close-btn");
+
+    menuIcon.addEventListener("click", function () {
+        sidebar.classList.add("active");
+    });
+
+    closeBtn.addEventListener("click", function () {
+        sidebar.classList.remove("active");
+    });
+});
+
+
+
