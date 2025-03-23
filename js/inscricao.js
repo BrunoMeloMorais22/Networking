41efeb97b6ab845c864a9c9dfa433a0e7ec3af37
Bruno <bruno9405@fito.br>
2025-03-23 15:34:38 -0300

    Atualizações

--- js/inscricao.js
+++ js/inscricao.js
@@ -5,21 +5,40 @@ document.getElementById("inscricaoFormulario").addEventListener("submit", functi
     let name = document.getElementById("name").value
     let email = document.getElementById("email").value
     let phone = document.getElementById("phone").value
+    let cpf = document.getElementById("cpd").value
+    let info = document.getElementById("info")
 
-    if(name === "" || email === "" || phone === ""){
+    info.innerHTML = ""
+
+    if(name === "" || email === "" || phone === "" || cpf === ""){
         let p = document.createElement("p")
-        p.textContent = "Inscrição enviada com sucesso"
-        p.style.color = "green"
+        p.textContent = "Prencha todos os espaços em branco"
+        p.style.color = "red"
         p.style.fontWeight = "bold"
         document.getElementById("info").appendChild(p)
     }
 
     else{
         let p2 = document.createElement("p")
+        let button = document.createElement("button")
         p2.textContent = "Inscrição enviada com sucesso."
+        button.style.padding = "10px"
+        button.textContent = "Voltar ao menu principal"
+        button.style.background = "green"
+        button.style.marginTop = "10px"
+        button.style.fontSize = "18px"
+        button.style.borderRadius = "20px"
+        button.style.cursor = "pointer"
+        button.style.border = "black"
+        button.style.fontFamily = "Courier New', Courier, monospace;"
         p2.style.color = "green"
         p2.style.fontSize = "20px"
         p2.style.fontWeight = "bold"
         document.getElementById("info").appendChild(p2)
+        document.getElementById("info").appendChild(button)
+
+        button.onclick = () =>{
+            window.location.href = "index2.html"
+        }
     }
 })
